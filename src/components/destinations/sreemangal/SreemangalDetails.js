e027f5849770b3b66d707af4010cfc7e7d56cae5
zubi <zubayerahmed232@gmail.com>
2020-09-17 19:42:53 +0600

    initial design done

--- src/components/destinations/sreemangal/SreemangalDetails.js
+++ src/components/destinations/sreemangal/SreemangalDetails.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Row, Col, Container, Figure } from "react-bootstrap";
 import { connect } from "react-redux";
-import {Redirect} from 'react-router-dom';
+//import {Redirect} from 'react-router-dom';
 import {useHistory} from 'react-router-dom';
 //import GoogleMap from "../../map/GoogleMap";
 import SreemangalMap from "./SreemangalMap";
@@ -22,62 +22,70 @@ const SreeMangalDetails = ({ data,userInfo }) => {
 			<Container>
 				<Row>
 					<Col lg={8} md={6} sm={12}>
-						<h3>Stay in {data.sajekDummyData.title}</h3>
+					
 						<Row>
-							<Col lg={4} md={3}>
+						
+							<Col lg={6} md={3} sm={6} >
+							<h3 >Stay in {data.sreemangalDummyData.title}</h3>
 								<Figure>
 									<Figure.Image
-										width={171}
-										height={180}
+										width={200}
+										height={400}
 										alt="171x180"
 										src="../../images/room2.png"
 									/>
 								</Figure>
 							</Col>
-							<Col lg={4} md={3}>
-								<h3>cool working</h3>
-							</Col>
+							<Col lg={2} md={3} sm={6}>
+							<p className="lead" style={{fontSize :'14px'}}> {data.sreemangalDummyData.hotel1.title}</p>
+							<p className="lead" style={{fontSize :'14px'}}> {data.sreemangalDummyData.hotel1.guests}</p>
+							
+							</Col> 
+							
 						</Row>
 
 						<Row>
-							<Col lg={4} md={3}>
+							<Col lg={6} md={3} sm={6}>
 								<Figure>
 									<Figure.Image
-										width={171}
-										height={180}
+										width={200}
+										height={400}
 										alt="171x180"
 										src="../../images/room3.png"
 									/>
 								</Figure>
 							</Col>
-							<Col lg={4} md={3}>
-								<h3>cool working</h3>
+							<Col lg={2} md={3} sm={6}>
+							<p className="lead" style={{fontSize :'14px'}}> {data.sreemangalDummyData.hotel2.title}</p>
+							<p className="lead" style={{fontSize :'14px'}}> {data.sreemangalDummyData.hotel2.guests}</p>
 							</Col>
 						</Row>
 						<Row>
-							<Col lg={4} md={3}>
+							<Col lg={6} md={3} sm={6}>
 								<Figure>
 									<Figure.Image
-										width={171}
-										height={180}
+										width={200}
+										height={400}
 										alt="171x180"
 										src="../../images/rooms4.png"
 									/>
 								</Figure>
 							</Col>
-							<Col lg={4} md={3}>
-								<h3>cool working</h3>
+							<Col lg={2} md={3} sm={6}>
+							<p className="lead" style={{fontSize :'14px'}}> {data.sreemangalDummyData.hotel3.title}</p>
+							<p className="lead" style={{fontSize :'14px'}}> {data.sreemangalDummyData.hotel3.guests}</p>
 							</Col>
 						</Row>
 					</Col>
 
 					<Col lg={4} md={6} sm={12}>
-						<h2>good job</h2>
+						<h2>Location</h2>
                         <SreemangalMap/>
 					</Col>
 				</Row>
 			</Container>
 		</div>
+
 	);
 };
 
--- src/components/destinations/sundarban/SundarbanDetails.js
+++ src/components/destinations/sundarban/SundarbanDetails.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Row, Col, Container, Figure } from "react-bootstrap";
 import { connect } from "react-redux";
 import SundarbanMap from "./SundarbanMap";
-import {Redirect} from 'react-router-dom';
+//import {Redirect} from 'react-router-dom';
 import {useHistory} from 'react-router-dom';
 //import GoogleMap from "../../map/GoogleMap";
 const SundarbanDetails = ({data,userInfo}) => {
@@ -22,62 +22,70 @@ const SundarbanDetails = ({data,userInfo}) => {
 			<Container>
 				<Row>
 					<Col lg={8} md={6} sm={12}>
-						<h3>Stay in {data.sajekDummyData.title}</h3>
+					
 						<Row>
-							<Col lg={4} md={3}>
+						
+							<Col lg={6} md={3} sm={6} >
+							<h3 >Stay in {data.sundarbanDummyData.title}</h3>
 								<Figure>
 									<Figure.Image
-										width={171}
-										height={180}
+										width={200}
+										height={400}
 										alt="171x180"
 										src="../../images/room2.png"
 									/>
 								</Figure>
 							</Col>
-							<Col lg={4} md={3}>
-								<h3>cool working</h3>
-							</Col>
+							<Col lg={2} md={3} sm={6}>
+							<p className="lead" style={{fontSize :'14px'}}> {data.sundarbanDummyData.hotel1.title}</p>
+							<p className="lead" style={{fontSize :'14px'}}> {data.sundarbanDummyData.hotel1.guests}</p>
+							
+							</Col> 
+							
 						</Row>
 
 						<Row>
-							<Col lg={4} md={3}>
+							<Col lg={6} md={3} sm={6}>
 								<Figure>
 									<Figure.Image
-										width={171}
-										height={180}
+										width={200}
+										height={400}
 										alt="171x180"
 										src="../../images/room3.png"
 									/>
 								</Figure>
 							</Col>
-							<Col lg={4} md={3}>
-								<h3>cool working</h3>
+							<Col lg={2} md={3} sm={6}>
+							<p className="lead" style={{fontSize :'14px'}}> {data.sundarbanDummyData.hotel2.title}</p>
+							<p className="lead" style={{fontSize :'14px'}}> {data.sundarbanDummyData.hotel2.guests}</p>
 							</Col>
 						</Row>
 						<Row>
-							<Col lg={4} md={3}>
+							<Col lg={6} md={3} sm={6}>
 								<Figure>
 									<Figure.Image
-										width={171}
-										height={180}
+										width={200}
+										height={400}
 										alt="171x180"
 										src="../../images/rooms4.png"
 									/>
 								</Figure>
 							</Col>
-							<Col lg={4} md={3}>
-								<h3>cool working</h3>
+							<Col lg={2} md={3} sm={6}>
+							<p className="lead" style={{fontSize :'14px'}}> {data.sundarbanDummyData.hotel3.title}</p>
+							<p className="lead" style={{fontSize :'14px'}}> {data.sundarbanDummyData.hotel3.guests}</p>
 							</Col>
 						</Row>
 					</Col>
 
 					<Col lg={4} md={6} sm={12}>
-						<h2>good job</h2>
+						<h2>Location</h2>
                         <SundarbanMap/>
 					</Col>
 				</Row>
 			</Container>
 		</div>
+
 	);
 };
 
